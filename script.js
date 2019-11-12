a6d5e73a2a9614d6c9adde63b13c92aa5da8254a
AriesNation <bradbotteron13@gmail.com>
2019-11-11 20:19:01 -0500

    js functionality to products page

--- script.js
+++ script.js
@@ -7,6 +7,11 @@
 var pages = ["About", "Products", "Services", "News", "Gallery"];
 var linkPages = ["about.html", "products.html", "services.html", "news.html", "gallery.html"];
 var logoHome = document.getElementById("logoAndName");
+/* Product variables for page creation */
+var productLeft = document.getElementById("productLeft");
+var productRight = document.getElementById("productRight");
+var products = ["T-Shirt", "Hoodies"];
+var sizes = ["SM", "M", "L", "XL", "XXL", "XXXL"];
 
 function createEventListeners()
 {
@@ -25,6 +30,8 @@ function generatePlaceHolders()
         var a = document.createElement("a");
         var pageText = document.createTextNode(pages[i]);
         a.href = linkPages[i];
+        a.id = pages[i];
+        console.log(a.id + "");
         a.appendChild(pageText);
         li.appendChild(a);
         nav.appendChild(li);
@@ -38,7 +45,29 @@ function generatePlaceHolders()
 
 function productList()
 {
-
+    console.log("*6. Entering productList()");
+    var ul = document.createElement("ul");
+    for(var i = 0; i < products.length; i++)
+    {
+        var ulSizes = document.createElement("ul");
+        var li = document.createElement("li");
+        li.style.fontSize = "1em";
+        li.style.color = "white";
+        li.id = products[i];
+        li.innerHTML = products[i];
+        ul.appendChild(li);
+        for(var j = 0; j < sizes.length; j++)
+        {
+            var liSizes = document.createElement("li");
+            liSizes.style.fontSize = ".75em";
+            liSizes.style.color = "white";
+            liSizes.id = sizes[j];
+            liSizes.innerHTML = sizes[j];
+            ul.appendChild(liSizes);
+        }
+    }
+    ul.id = "productListContent";
+    productLeft.appendChild(ul);
 }
 
 function init()
@@ -46,6 +75,7 @@ function init()
     console.log("*2. Entering setUpPage -> Create event listeners && generatePlaceHolders");
     createEventListeners();
     generatePlaceHolders();
+    productList();
 }
 
 if(window.addEventListener)
