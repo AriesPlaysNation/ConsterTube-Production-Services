dc5515b43e68c2121077f57134c25613aeedc224
AriesNation <bradbotteron13@gmail.com>
2019-11-26 14:49:37 -0500

    added gallery script

---
+++ scripts/galleryScript.js
@@ -0,0 +1,30 @@
+/* Author:          Brad Botteron
+   Date Written:    10/22/19
+   Description:     Gallery Script JavaScript
+ */
+
+
+
+function generatePictures()
+{
+
+}
+
+function init()
+{
+    generatePictures();
+}
+
+if(window.addEventListener)
+{
+    window.addEventListener("load", init, false);
+} else if (window.attachEvent)
+{
+    window.attachEvent("onload", init);
+}
+
+
+
+
+
+
