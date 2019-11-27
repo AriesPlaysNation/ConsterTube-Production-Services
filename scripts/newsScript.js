75c49ee9849fddbfbe6e956d575e535cd9405f74
AriesNation <bradbotteron13@gmail.com>
2019-11-27 09:19:08 -0500

    attempting to add facebook feed

---
+++ scripts/newsScript.js
@@ -0,0 +1,39 @@
+/* Author:          Brad Botteron
+   Date Written:    11/25/19
+   Description:     News script -> JavaScript <-
+ */
+
+var feed = document.getElementById("facebookFeed");
+
+
+
+function generateFeed()
+{
+
+}
+
+function init()
+{
+    generateFeed();
+}
+
+if(window.addEventListener)
+{
+    window.addEventListener("load", init, false);
+} else if (window.attachEvent)
+{
+    window.attachEvent("onload", init);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
