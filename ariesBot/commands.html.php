4b1ec9591bab6b081565f66be64a1e0fd043d416
AriesNation <bradbotteron13@gmail.com>
2020-02-09 20:37:07 -0500

    nav inline issues

--- ariesBot/commands.html.php
+++ ariesBot/commands.html.php
@@ -5,7 +5,7 @@ require ("htmlHead.inc");
     <header>
         <nav id="nav">
             <ul>
-                <li class="navA"><a href="home.html.php"><img src="img/ProPic2.png" alt="Profile Image" width="35px" height="35px"></a> </li>
+                <li class="navA"><a href="home.html.php">Aries Nation Bot</a> </li>
                 <li class="navA"><a href="home.html.php">Home</a></li>
                 <li class="navA"><a href="commands.html.php">Commands</a> </li>
             </ul>
