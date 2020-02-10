1957b109fa37dc4f09606d31063f429fb16cafc9
AriesNation <bradbotteron13@gmail.com>
2020-02-09 19:44:17 -0500

    added php require footer and added .php extension

---
+++ bot/commands.html.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/botStyle.css"/>
+    <title>Aries Bot Commands</title>
+</head>
+<body>
+<div id="wrapper">
+    <header>
+        <nav id="nav">
+            <ul>
+                <li class="navA"><a href="home.html.php"><img src="img/ProPic2.png" alt="Profile Image" width="35px" height="35px"></a> </li>
+                <li class="navA"><a href="home.html.php">Home</a></li>
+                <li class="navA"><a href="commands.html.php">Commands</a> </li>
+            </ul>
+        </nav>
+    </header>
+
+<?php require ("htmlFoot.inc");?>
