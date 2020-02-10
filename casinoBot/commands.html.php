4bee8c7e37e4ca0e4f0d05882d6b0be13333e855
AriesNation <bradbotteron13@gmail.com>
2020-02-09 20:03:54 -0500

    added 3rd subwebsite for casino bot

---
+++ casinoBot/commands.html.php
@@ -0,0 +1,15 @@
+<?php
+$title = "Aries Commands";
+require ("htmlHead.inc");
+?>
+    <header>
+        <nav id="nav">
+            <ul>
+                <li class="navA"><a href="home.html.php"><img src="img/ProPic2.png" alt="Profile Image" width="35px" height="35px"></a> </li>
+                <li class="navA"><a href="home.html.php">Home</a></li>
+                <li class="navA"><a href="commands.html.php">Commands</a> </li>
+            </ul>
+        </nav>
+    </header>
+
+<?php require ("htmlFoot.inc");?>
