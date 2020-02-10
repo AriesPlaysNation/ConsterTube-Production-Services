aad15cb1e1d5bedb7e0c04b80df0dcf70c24c62f
AriesNation <bradbotteron13@gmail.com>
2020-02-09 20:45:26 -0500

    added nav to htmlHead.inc

--- ariesBot/commands.html.php
+++ ariesBot/commands.html.php
@@ -2,14 +2,5 @@
 $title = "Aries Commands";
 require ("htmlHead.inc");
 ?>
-    <header>
-        <nav id="nav">
-            <ul>
-                <li class="navA"><a href="home.html.php">Aries Nation Bot</a> </li>
-                <li class="navA"><a href="home.html.php">Home</a></li>
-                <li class="navA"><a href="commands.html.php">Commands</a> </li>
-            </ul>
-        </nav>
-    </header>
 
 <?php require ("htmlFoot.inc");?>
--- ariesBot/home.html.php
+++ ariesBot/home.html.php
@@ -4,14 +4,4 @@ $title = "Aries Bot Home";
 require("htmlHead.inc");
 ?>
 
-<header>
-    <nav id="nav">
-        <ul>
-            <li class="navA"><a href="home.html.php">Aries Nation Bot</a> </li>
-            <li class="navA"><a href="home.html.php">Home</a></li>
-            <li class="navA"><a href="commands.html.php">Commands</a> </li>
-        </ul>
-    </nav>
-</header>
-
 <?php require ("htmlFoot.inc");?>
