316332f7e7edba7be7c1afd0faaa6eab48fd3095
AriesNation <bradbotteron13@gmail.com>
2020-02-09 20:36:00 -0500

    nav inline issues

--- ariesBot/home.html.php
+++ ariesBot/home.html.php
@@ -6,8 +6,8 @@ require("htmlHead.inc");
 
 <header>
     <nav id="nav">
-        <span>Aries Nation Discord Bot</span>
         <ul>
+            <li class="navA"><a href="home.html.php"><img src="img/ProPic2.png" alt="Profile Image" width="35px" height="35px"></a> </li>
             <li class="navA"><a href="home.html.php">Home</a></li>
             <li class="navA"><a href="commands.html.php">Commands</a> </li>
         </ul>
