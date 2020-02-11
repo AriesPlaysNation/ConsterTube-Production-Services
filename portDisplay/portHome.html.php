06d87ab1148ebd39c6c0687d7db8dece9d9d4415
AriesNation <bradbotteron13@gmail.com>
2020-02-11 18:29:52 -0500

    attempting table

--- portDisplay/portHome.html.php
+++ portDisplay/portHome.html.php
@@ -6,5 +6,9 @@ $title = "Portfolio Home Page";
 ?>
 
 <table id="indexList">
-    <span id="listContent"</span>
+    <!--<span id="listContent"</span>-->
+    <tr>
+        <th>blah blah</th>
+        <th>blah blah</th>
+    </tr>
 </table>
