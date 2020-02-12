7b9d5b765f70ed8b8d3533d47adcb39ea3a2afe0
AriesNation <bradbotteron13@gmail.com>
2020-02-12 18:36:26 -0500

    css to admin.css and adding first span

--- admin/admin.html.php
+++ admin/admin.html.php
@@ -3,6 +3,11 @@ $title = "Admin Home";
 require ("header.inc");
 ?>
 
-
+<div id="columnContent">
+    <span class="contentSections">
+        <h2>ConsterTube</h2><br>
+        <a href="../index.html">Index.html</a>
+    </span>
+</div>
 
 <?php require ("footer.inc");?>
