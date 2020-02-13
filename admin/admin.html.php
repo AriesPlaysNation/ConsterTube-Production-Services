2295739e84206a6bbfeffb2ebe308c26bb06ee1b
AriesNation <bradbotteron13@gmail.com>
2020-02-12 21:35:10 -0500

    css to admin.css

--- admin/admin.html.php
+++ admin/admin.html.php
@@ -6,7 +6,11 @@ require ("header.inc");
 <div id="columnContent">
     <span class="contentSections">
         <h2>ConsterTube</h2><br>
-        <a href="../index.html">Index.html</a>
+        <a href="../index.html">Site</a>
+    </span>
+    <span class="contentSections">
+        <h2>Aries Discord Bot</h2>
+        <a href="../ariesBot/home.html.php">Site</a>
     </span>
 </div>
 
