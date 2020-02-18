15cb5c3e144900c7e38b45428d607d7e6a6013b3
AriesNation <bradbotteron13@gmail.com>
2020-02-18 13:49:54 -0500

    db test for m14

--- portDisplay/JavaScript/modules/M14/connecti2db.inc.php
+++ portDisplay/JavaScript/modules/M14/connecti2db.inc.php
@@ -6,7 +6,7 @@
   #				before requiring this file.
 
     // Assign variables
-    $host     = "ssh.sullens.net";		// must use 127.0.0.1 with mysqli
+    $host     = "sullens.net";		// must use 127.0.0.1 with mysqli
     $uname    = "ajacks";	
     $pass     = "BR2wRtMSdh6ESHtY";
 
